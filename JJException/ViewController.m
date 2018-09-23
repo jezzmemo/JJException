7f685bb8445f342585e35bedd1714b198422428c
李杰 <jezz.li@hujiang.com>
2018-09-23 15:13:29 +0800

    add demo code

--- JJException/ViewController.m
+++ JJException/ViewController.m
@@ -21,23 +21,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIButton* startGuardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [startGuardButton setTitle:@"Start Guard" forState:UIControlStateNormal];
+    [startGuardButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    startGuardButton.frame = CGRectMake((self.view.frame.size.width - 100)/2, 200, 100, 50);
+    [startGuardButton addTarget:self action:@selector(startGuardAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:startGuardButton];
     
-    JJException.exceptionWhenTerminate = NO;
+    UIButton* stopGuardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [stopGuardButton setTitle:@"Stop Guard" forState:UIControlStateNormal];
+    [stopGuardButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    stopGuardButton.frame = CGRectMake((self.view.frame.size.width - 100)/2, 250, 100, 50);
+    [stopGuardButton addTarget:self action:@selector(stopGuardAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:stopGuardButton];
     
-    [JJException configExceptionCategory:JJExceptionGuardAll];
-    [JJException startGuardException];
-    
-    [JJException registerExceptionHandle:self];
-    
-    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button setTitle:@"Push controller" forState:UIControlStateNormal];
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"Test KVO|NotificatinCenter|NSTimer" forState:UIControlStateNormal];
     [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    button.frame = CGRectMake(100, 100, self.view.frame.size.width, 50);
-    button.center = self.view.center;
-    [button addTarget:self action:@selector(pushAction) forControlEvents:UIControlEventTouchUpInside];
+    button.frame = CGRectMake(0, 300, self.view.frame.size.width, 50);
+    [button addTarget:self action:@selector(testKVONotificatinCenterNSTimerAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
+    
+    UIButton* otherButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [otherButton setTitle:@"Test NSArray|NSDictionary|UnrecognizedSelector" forState:UIControlStateNormal];
+    [otherButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    otherButton.frame = CGRectMake(0, 350, self.view.frame.size.width, 50);
+    [otherButton addTarget:self action:@selector(testArrayDictionaryUnrecognizedSelector) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:otherButton];
 }
-#pragma mark - Delegate
+#pragma mark - Exception Delegate
 
 - (void)handleCrashException:(NSString*)exceptionMessage extraInfo:(NSDictionary*)info{
     
@@ -45,9 +57,45 @@
 
 #pragma mark - Action
 
-- (void)pushAction{
+- (void)startGuardAction{
+    JJException.exceptionWhenTerminate = NO;
+    
+    [JJException configExceptionCategory:JJExceptionGuardAll];
+    [JJException startGuardException];
+    
+    [JJException registerExceptionHandle:self];
+}
+
+- (void)stopGuardAction{
+    [JJException stopGuardException];
+}
+
+#pragma mark - Test Action
+
+- (void)testKVONotificatinCenterNSTimerAction{
     PushViewController* push = [[PushViewController alloc] init];
     [self presentViewController:push animated:YES completion:nil];
+}
+
+- (void)testArrayDictionaryUnrecognizedSelector{
+    [self testSampleArray];
+    [self testSimpleDictionary];
+    [self testUnrecognizedSelector];
+}
+
+- (void)testSampleArray{
+    NSArray* test = @[];
+    NSLog(@"object:%@",test[1]);
+}
+
+- (void)testSimpleDictionary{
+    id value = nil;
+    NSDictionary* dic = @{@"key":value};
+    NSLog(@"dic:%@",dic);
+}
+
+- (void)testUnrecognizedSelector{
+    [self performSelector:@selector(testUndefineSelector)];
 }
 
 
