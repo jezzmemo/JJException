b2233becd90a481e5a99f6cc108eb2d8e5504c61
李杰 <jezz.li@hujiang.com>
2019-01-23 22:15:26 +0800

    support the UnrecognizedSelector for the class method

--- JJException/Source/MRC/NSObject+UnrecognizedSelectorHook.m
+++ JJException/Source/MRC/NSObject+UnrecognizedSelectorHook.m
@@ -14,8 +14,23 @@
 @implementation NSObject (UnrecognizedSelectorHook)
 
 + (void)jj_swizzleUnrecognizedSelector{
+    
+    //Class Method
+    swizzleClassMethod([self class], @selector(methodSignatureForSelector:), @selector(classMethodSignatureForSelectorSwizzled:));
+    swizzleClassMethod([self class], @selector(forwardInvocation:), @selector(forwardClassInvocationSwizzled:));
+    
+    //Instance Method
     swizzleInstanceMethod([self class], @selector(methodSignatureForSelector:), @selector(methodSignatureForSelectorSwizzled:));
     swizzleInstanceMethod([self class], @selector(forwardInvocation:), @selector(forwardInvocationSwizzled:));
+}
+
++ (NSMethodSignature*)classMethodSignatureForSelectorSwizzled:(SEL)aSelector {
+    NSMethodSignature* methodSignature = [self classMethodSignatureForSelectorSwizzled:aSelector];
+    if (methodSignature) {
+        return methodSignature;
+    }
+    
+    return [self.class checkObjectSignatureAndCurrentClass:self.class];
 }
 
 - (NSMethodSignature*)methodSignatureForSelectorSwizzled:(SEL)aSelector {
@@ -24,8 +39,20 @@
         return methodSignature;
     }
     
+    return [self.class checkObjectSignatureAndCurrentClass:self.class];
+}
+
+/**
+ * Check the class method signature to the [NSObject class]
+ * If not equals,return nil
+ * If equals,return the v@:@ method
+
+ @param currentClass Class
+ @return NSMethodSignature
+ */
++ (NSMethodSignature *)checkObjectSignatureAndCurrentClass:(Class)currentClass{
     IMP originIMP = class_getMethodImplementation([NSObject class], @selector(methodSignatureForSelector:));
-    IMP currentClassIMP = class_getMethodImplementation(self.class, @selector(methodSignatureForSelector:));
+    IMP currentClassIMP = class_getMethodImplementation(currentClass, @selector(methodSignatureForSelector:));
     
     // If current class override methodSignatureForSelector return nil
     if (originIMP != currentClassIMP){
@@ -37,8 +64,23 @@
     return [NSMethodSignature signatureWithObjCTypes:"v@:@"];
 }
 
+/**
+ Forward instance object
+
+ @param invocation NSInvocation
+ */
 - (void)forwardInvocationSwizzled:(NSInvocation*)invocation{
-    NSString* message = [NSString stringWithFormat:@"Unrecognized selector class:%@ and selector:%@",NSStringFromClass(self.class),NSStringFromSelector(invocation.selector)];
+    NSString* message = [NSString stringWithFormat:@"Unrecognized instance class:%@ and selector:%@",NSStringFromClass(self.class),NSStringFromSelector(invocation.selector)];
+    handleCrashException(JJExceptionGuardUnrecognizedSelector,message);
+}
+
+/**
+ Forward class object
+
+ @param invocation NSInvocation
+ */
++ (void)forwardClassInvocationSwizzled:(NSInvocation*)invocation{
+    NSString* message = [NSString stringWithFormat:@"Unrecognized static class:%@ and selector:%@",NSStringFromClass(self.class),NSStringFromSelector(invocation.selector)];
     handleCrashException(JJExceptionGuardUnrecognizedSelector,message);
 }
 
