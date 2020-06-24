543865434e6499153cab40f5ff06b0c1527605d7
张雅琼 <yaqiong_zhang@insig.net>
2020-06-24 17:58:33 +0800

    判空处理。

--- JJException/Source/MRC/NSObject+KVOCrash.m
+++ JJException/Source/MRC/NSObject+KVOCrash.m
@@ -42,7 +42,11 @@ static const char DeallocKVOKey;
 }
 
 - (BOOL)isEqual:(KVOObjectItem*)object{
-    // self.observer可能已释放
+    // 防止可能已释放
+    if (!self.observer || !self.whichObject || !self.keyPath
+        || !object.observer || !object.whichObject || !object.keyPath) {
+        return NO;
+    }
     if ([self.observer isEqual:object.observer] && [self.whichObject isEqual:object.whichObject] && [self.keyPath isEqualToString:object.keyPath]) {
         return YES;
     }
